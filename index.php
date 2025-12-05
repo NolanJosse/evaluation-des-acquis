90d83e0fb462847a6657f38300f45571030fd76d
NolanJosse <nolan.josse2006@gmail.com>
2025-12-05 15:16:27 +0100

    main section1 et 2

--- index.php
+++ index.php
@@ -12,4 +12,43 @@
             <a href="#">Contactez-moi</a>
         </nav>
     </header>
+
+<section id="section1">
+    <div class="row">
+        <img src="https://picsum.photos/200/150" alt="random">
+        <p>ceci est un test</p>
+        <p>je suis en bts sio</p>
+    </div>
+</section>
+
+<section id="section2">
+    <div class="row">
+
+        <img src="https://picsum.photos/200/150" alt="random">
+
+        <form method="POST">
+            <p>Nom : <input type="text" name="nom" required></p>
+            <p>Email : <input type="email" name="email" required></p>
+
+            <button type="submit">Validez</button>
+
+            <?php 
+                if (!empty($_POST)) {
+
+                    $nom = trim($_POST['nom']);
+                    $email = trim($_POST['email']);
+
+                    if ($nom !== "" && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        echo '<p class="message success">Votre demande est bien envoyée !</p>';
+                    } else {
+                        echo '<p class="message error">Vos informations ne sont pas correctes</p>';
+                    }
+                }
+            ?>
+        </form>
+
+    </div>
+</section>
+
+
 </body>
